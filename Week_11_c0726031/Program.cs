bd3456943905f2773b45c34969cb1eb00198fb70
visualstudio <visualstudio@122-PC34>
2019-03-29 21:40:15 -0400

    download

--- Week_11_c0726031/Program.cs
+++ Week_11_c0726031/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace Week_11_c0726031
@@ -10,6 +11,31 @@ namespace Week_11_c0726031
     {
         static void Main(string[] args)
         {
+            {
+
+                Console.WriteLine("Downloading file");
+                Download();
+                Console.ReadLine();
+            }
+
+            static async void Download()
+            {
+                await Network.Download();
+                Console.WriteLine("Download complete");
+            }
+
+        class Network
+        {
+            public static async Task Download() {
+
+                HttpClient client = new HttpClient();
+                var data = await client.GetStringAsync("htttp://torontopubliclibrary.ca");
+                Console.WriteLine(data);
+            }
         }
     }
 }
+
+        
+    
+
